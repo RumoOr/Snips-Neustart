e06b1af29e1ab6c424253ebf63cd3a00dda01bfc
Johannes Jahn <jojahn@gmx.de>
2018-10-13 16:09:51 +0200

    Minor changes

--- action-question-WolframAlpha.py
+++ action-question-WolframAlpha.py
@@ -39,4 +39,4 @@ def action_wrapper(hermes, intentMessage):
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("RumoOr:restart", subscribe_intent_callback).start()
+        h.subscribe_intent("RumoOr:question", subscribe_intent_callback).start()
