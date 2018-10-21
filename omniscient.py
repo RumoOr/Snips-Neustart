6f24da9ce6d71c19565e3eb9522508f603235f9b
Johannes Jahn <jojahn@gmx.de>
2018-10-21 10:30:57 +0200

    Minor changes

--- omniscient.py
+++ omniscient.py
@@ -25,11 +25,27 @@ class Omniscient:
             self.google_key_path = "XXXXXXXXXX"            
         credentials = service_account.Credentials.from_service_account_file(self.google_key_path) 
         self.translator = translate.Client(credentials=credentials)
+        self.wolfram = wolframalpha.Client(self.wolfram_key)
 
     def get_answer(self, question):
         try: 
-            question = self.translator.translate("input message " + question, target_language='de')
-            return question['translatedText']
+            question = self.translator.translate('wie ist das wetter in leipzig', target_language='en')
+            res = self.wolfram.query(question['translatedText'])
+            text = TEXT_QUESTION_ERROR
+            if res['@success'] == 'true':
+                try:
+                    text = next(res.results, text).text         
+                    text = self.translator.translate(text, source_language='en', target_language='de')['translatedText']
+                    text = text.replace(' |' , ',')
+                    for c in ['(', '[']:
+                        if c in text: 
+                            text = next(iter(text.split(c, 1)))
+                    text = text.rstrip()
+                    if (text == ''): 
+                        text = TEXT_ANSWER_ERROR
+                except:
+                    text = TEXT_ANSWER_ERROR
+            return text
         except:
             return TEXT_QUESTION_ERROR  
   
